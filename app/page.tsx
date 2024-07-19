4878db8540173eedb82588d0f1b998d7a1ebef9d
Susana Castillo <71852527+Susanelizabetth@users.noreply.github.com>
2024-07-19 00:15:43 +0000

    Pending changes exported from your codespace

--- app/page.tsx
+++ app/page.tsx
@@ -22,48 +22,57 @@ export default function Home() {
     console.log("Respuesta del servidor:");
   };
 
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleButtonClick();
+    }
+  };
+
   return (
     <main className="">
-      <div className="absolute h-screen w-screen bg-amber-100">
-        <div className="w-36 h-36 relative bg-orange-400 top-24 left-24 rounded-full blur-xl"></div>
-        <div className="relative w-96 h-96 bg-orange-300 top-56 left-72 rounded-full blur-xl"></div>
-        <div className="relative w-96 h-96 bg-orange-300 bottom-[200px] left-[800px] rounded-full blur-xl"></div>
-      </div>
-      <div className="h-[95vh] flex flex-col w-full m-5 px-10 pb-10 pt-4 backdrop-blur-3xl rounded-lg fixed bg-black/30">
-        <Header
-          title="ChatBot"
-          className="text-2xl font-extrabold text-orange-500 bg-white/70 p-2 rounded-xl"
-        />
-        <div id="chats" className="grow flex flex-col-reverse gap-2 mb-5">
-          {chats
-            .slice()
-            .reverse()
-            .map((chat, index) => (
-              <div
-                className={
-                  chat.type == 1
-                    ? "bg-gray-300 rounded-lg text-start p-2 w-max self-end"
-                    : "rounded-lg text-end p-2 w-max bg-orange-200/50"
-                }
-                key={index}
-              >
-                <span>{chat.text}</span>
-              </div>
-            ))}
+      <div className="absolute h-screen w-screen bg-black  flex justify-center">
+        <div className="bg-gradient-to-tr from-orange-900 to-purple-900 rounded-full w-[50vw] h-[70vh] self-center flex justify-center">
+          <div className="self-center w-1/2 h-[600px] bg-black rounded-full"></div>
         </div>
-        <div className="flex gap-2 w-full ">
-          <input
-            className="grow rounded-lg p-2"
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
+      </div>
+      <div className="w-full h-[100vh] flex justify-center">
+        <div className="h-[95vh] w-[95vw] self-center flex flex-col px-10 pb-10 pt-4 backdrop-blur-3xl rounded-lg fixed bg-white/15 shadow-xl shadow-gray-500/50">
+          <Header
+            title="ChatBot"
+            className="text-2xl font-extrabold text-orange-500 bg-black/70 p-2 rounded-xl"
           />
-          <button
-            onClick={handleButtonClick}
-            className="rounded-lg bg-orange-400 p-2"
-          >
-            Enviar
-          </button>
+          <div id="chats" className="grow flex flex-col-reverse gap-2 mb-5 overflow-y-auto">
+            {chats
+              .slice()
+              .reverse()
+              .map((chat, index) => (
+                <div
+                  className={
+                    chat.type == 1
+                      ? "bg-gray-700 text-white rounded-lg text-start p-2 w-max self-end"
+                      : "rounded-lg text-white text-end p-2 w-max bg-orange-600/50"
+                  }
+                  key={index}
+                >
+                  <span>{chat.text}</span>
+                </div>
+              ))}
+          </div>
+          <div className="flex gap-2 w-full ">
+            <input
+              className="grow rounded-lg p-2"
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              onKeyPress={handleKeyPress}
+            />
+            <button
+              onClick={handleButtonClick}
+              className="rounded-lg bg-orange-600 p-2 text-white"
+            >
+              Enviar
+            </button>
+          </div>
         </div>
       </div>
     </main>
